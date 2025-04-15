fd2863b4693ac5533ad96a36aae4d26963ea7815
tmdtmddl <tmddlqkqh@naver.com>
2025-04-15 23:07:28 +0900

    naver map api test

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+// "use client";
+
+// declare global {
+//   interface Window {
+//     naver: any;
+//   }
+// }
+// const App = () => {
+//   const mapOptions = {
+//     center: new naver.maps.LatLng(37.3595704, 127.105399),
+//     zoom: 10,
+//   };
+//   const map = new naver.maps.Map("map", mapOptions); // id와 option
+//   return (
+//     <div>
+//       <div id="map" style={{ width: "100%", height: "400px" }}></div>;
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,32 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { GiBoarTusks, GiDeerHead, GiDeerTrack } from "react-icons/gi";
-import { useTransition } from "react";
+import { useEffect, useRef } from "react";
 
-import { twMerge } from "tailwind-merge";
-import RootLoading from "./loading";
-
-const WildPage = () => {
-  const navi = useRouter();
-  const [isPending, startTransition] = useTransition();
+const Map = () => {
+  console.log(process.env.NEXT_PUBLIC_API_KEY);
+  const mapRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.naver && mapRef.current) {
+      const location = new naver.maps.LatLng(37.5666103, 126.9783882);
+      const mapOptions = {
+        // 지도의 초기 중심 좌표
+        center: location,
+        zoom: 14, // 지도의 초기 줌 레벨
+      };
+      new naver.maps.Map(mapRef.current, mapOptions);
+    }
+  }, []);
 
   return (
-    <div className=" h-screen relative bg-linear-to-t from-lime-500 to-lime-800">
-      {isPending && <RootLoading />}
-      <div className="relative top-[40%] hover:animate-pulse ">
-        <button
-          onClick={() => navi.push("/wild/wId")}
-          className="flex flex-col cursor-pointer flex-1 p-5 w-full  relative bg-white min-w-80 max-w-100 mx-auto  rounded  items-center  border-2 border-teal-950 hover:shadow-md hover:shadow-green-800 hover:border-teal-800 "
-        >
-          <p className="font-bold text-3xl  hover:text-teal-800">
-            유해야생동물 포획 현황
-          </p>
-
-          <div className="text-7xl -rotate-12 absolute top-[-90%] right-95  ">
-            <GiBoarTusks />
-          </div>
-
-          <div className="text-7xl rotate-12 absolute top-[85px] left-90 ">
-            <GiDeerHead />
-          </div>
-        </button>
-      </div>
-
-      <div className="text-3xl relative top-100">
-        <GiDeerTrack className={twMerge(icons, "left-18")} />
-        <GiDeerTrack className={twMerge(icons, "top-10 left-10")} />
-        <GiDeerTrack className={twMerge(icons, "top-20 left-3")} />
-      </div>
-      <div className="text-3xl  ">
-        <GiDeerTrack className={twMerge(icons, "top-2 right-5")} />
-        <GiDeerTrack className={twMerge(icons, "top-10  right-15")} />
-        <GiDeerTrack className={twMerge(icons, "top-20  right-25")} />
-      </div>
+    <div>
+      <div
+        id="map"
+        ref={mapRef}
+        className="max-w-[1200px] mx-auto"
+        style={{ width: "1143px", height: "594px" }}
+      />
     </div>
   );
 };
 
-export default WildPage;
-
-const icons = "absolute rotate-12";
+export default Map;
